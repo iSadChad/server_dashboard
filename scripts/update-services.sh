716ca09f8c16140e8f5fbbd1de129b7596ad48c3
SadTset <tenzin.sharlhey@lernende.bbw.ch>
2026-08-11 12:23:46 +0200

    feat: refactor update_docker function for improved logging and clarity

--- scripts/update-services.sh
+++ scripts/update-services.sh
@@ -3,16 +3,33 @@
 
 set -Eeuo pipefail
 
+declare -a DOCKER_PROJECTS=(
+  "$HOME/docker/BumTeacherBypass"
+  "$HOME/docker/actual-budget"
+  "$HOME/docker/adguardhome"
+  "$HOME/docker/snapotter"
+  "$HOME/docker/stirling-pdf"
+)
 
 log() {
   printf '[%s] %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$1"
 }
 
 update_docker() {
-  log "Updating Docker images..."
-  docker compose pull
-  docker compose up -d
-  log "Docker images updated."
+  log "Starting Docker updates..."
+
+  for projects in "${DOCKER_PROJECTS[@]}"; do 
+    cd "${projects}"
+
+    log "Updating Docker project: ${projects}"
+
+    docker compose pull
+    docker compose up -d
+
+    log "Finished updating Docker project: ${projects}"
+  done
+
+  log "All Docker projects updated."
 }
 
 update_ubuntu() {
@@ -20,3 +37,10 @@ update_ubuntu() {
   sudo apt update && sudo apt upgrade -y
   log "Ubuntu packages updated."
 }
+
+main(){
+  update_docker
+  update_ubuntu
+}
+
+main
